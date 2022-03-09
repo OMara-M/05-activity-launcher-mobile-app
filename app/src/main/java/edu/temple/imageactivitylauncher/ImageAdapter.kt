11d991adc750184654d15b5d82780e73035b5380
Matthew O'Mara <tuh39725@temple.edu>
2022-03-09 16:15:58 -0500

    update ImageAdapter

--- app/src/main/java/edu/temple/imageactivitylauncher/ImageAdapter.kt
+++ app/src/main/java/edu/temple/imageactivitylauncher/ImageAdapter.kt
@@ -2,35 +2,43 @@ package edu.temple.imageactivitylauncher
 
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ImageAdapter(_imageDataItems: Array<ImageData>, _handleClick: (ImageData) -> Unit) :
     RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
 
-    private val imageDataItems = _imageDataItems
+    private val items = _imageDataItems
     val handleClick = _handleClick
 
-    inner class ViewHolder(_view: ImageView) : RecyclerView.ViewHolder(_view) {
-        lateinit var item: ImageData
-        val imageView = _view.apply {
-            setOnClickListener { handleClick(item) }
+    class ViewHolder(_layout: LinearLayout) : RecyclerView.ViewHolder(_layout) {
+        val layout = _layout
+        val imageView = layout.getChildAt(0) as ImageView
+        val textView = layout.getChildAt(1) as TextView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageAdapter.ViewHolder {
+        val linearLayout = LinearLayout(parent.context)
+        with (linearLayout) {
+            orientation = LinearLayout.VERTICAL
+            addView(ImageView(parent.context).apply { layoutParams = ViewGroup.LayoutParams(300, 300) })
+            addView(TextView(parent.context).apply { textSize = 22f })
         }
+
+        return ViewHolder(linearLayout)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(ImageView(parent.context).apply {
-            layoutParams = ViewGroup.LayoutParams(200, 200)
-        })
-    }
+    override fun onBindViewHolder(holder: ImageAdapter.ViewHolder, position: Int) {
+        holder.imageView.setImageResource(items[position].resourceId)
+        holder.textView.text = items[position].description
 
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.imageView.setImageResource(imageDataItems[position].resourceId)
-        holder.item = imageDataItems[position]
+        // Assign on click listener when binding
+        holder.layout.setOnClickListener{handleClick(items[position])}
     }
 
     override fun getItemCount(): Int {
-        return imageDataItems.size
+        return items.size
     }
 
 }
